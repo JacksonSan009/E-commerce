20384daf50774589f705d034b0c49d6f8ea7993d
Jackson <maxtermax4@gmail.com>
2021-06-22 22:46:41 -0500

    Documentation

--- src/main/java/com/example/ecommerce/controller/FileUploadUtil.java
+++ src/main/java/com/example/ecommerce/controller/FileUploadUtil.java
@@ -9,20 +9,30 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 
+/**
+ * This class is in charge of upload the archives and create the main folder where the data is saved.
+ * */
 public class FileUploadUtil {
-	public static void saveFile(String uploadDir, String fileName,
-								MultipartFile multipartFile) throws IOException {
-		Path uploadPath = Paths.get(uploadDir);
 
-		if (!Files.exists(uploadPath)) {
-			Files.createDirectories(uploadPath);
-		}
+  /**
+   * @param uploadDir This is the route where the archive.
+   * @param fileName Name how is gonna be saved.
+   * @param multipartFile The archive.
+   * @throws IOException If the archive can't be saved and show a message with the name of the archive.
+   */
+  public static void saveFile(String uploadDir, String fileName, MultipartFile multipartFile)
+      throws IOException {
+    Path uploadPath = Paths.get(uploadDir);
 
-		try (InputStream inputStream = multipartFile.getInputStream()) {
-			Path filePath = uploadPath.resolve(fileName);
-			Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
-		} catch (IOException ioe) {
-			throw new IOException("Could not save file: " + fileName, ioe);
-		}
-	}
+    if (!Files.exists(uploadPath)) {
+      Files.createDirectories(uploadPath);
+    }
+
+    try (InputStream inputStream = multipartFile.getInputStream()) {
+      Path filePath = uploadPath.resolve(fileName);
+      Files.copy(inputStream, filePath, StandardCopyOption.REPLACE_EXISTING);
+    } catch (IOException ioe) {
+      throw new IOException("Could not save file: " + fileName, ioe);
+    }
+  }
 }
